f92f8e8c1bf8d6916ccee7e7d7b7a01209e49c5e
Adrian Pastore <adrianqpastore@gmail.com>
2017-10-01 00:19:51 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const divResultado = document.querySelector('div#resultado');
+const scriptTemplate = document.querySelector('#template');
+
+form.addEventListener('submit', function(e) {
+    busca(form.cep.value);
+    e.preventDefault();
+});
+
+function ajax(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onload = callback;
+  xhr.send();
+}
+
+function busca(cep) { // cep: 96201460
+  const url = `http://viacep.com.br/ws/${cep}/json/`;
+  ajax(url, function(e) {
+    printa(JSON.parse(e.target.response));
+  });
+}
+
+function printa(json) {
+  const template = scriptTemplate.innerText;
+  const handlebars = Handlebars.compile(template);
+  const html = handlebars(json);
+  divResultado.innerHTML = html;
+}
+function limpaformcep() {
+  divResultado.innerHTML = '';
+}
